34b3ec9717da6c26f864e1b27bf49e79725662bb
Pietro Kaynan Carvalho Lopes da Silva <pietro.k@aluno.ifsp.edu.br>
2024-10-04 19:48:40 -0300

    arrumei o projeto (eu acho)

--- ifpizza/src/main/java/edu/ifsp/ifpizza/modelo/Ingrediente.java
+++ ifpizza/src/main/java/edu/ifsp/ifpizza/modelo/Ingrediente.java
@@ -1,11 +1,37 @@
-package edu.ifsp.ifpizza.modelo;
+package ifsp.edu.IFPizza.model;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class Ingrediente {
 	private final String id;
 	private final String nome;
 	private final Tipo tipo;
 	
-	public enum Tipo{
-		BORDA, PROTEINA, VEGETAIS, QUEIJO
+	public Ingrediente(String id, String nome, Tipo tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.tipo = tipo;
 	}
+
+	public enum Type {
+			BORDA, PROTEINA, VEGETAIS, QUEIJO
+		}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return nome;
+	}
+
+	public Type getType() {
+		return tipo;
+	}
+	
+	
 }
